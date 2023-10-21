81e348d7d6056016b4fb94b040afd1ffedfaa2df
thully <thully.developer@gmail.com>
2023-10-21 03:23:04 +0200

    anime site

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    context = {
+        "page": "home",
+    }
+
+    return render(request, "app/pages/home.html", context)
+
